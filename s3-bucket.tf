186dc2692f1bbcb4743525a553f942b269d32ef8
Aparajithan Vaidyanathan <aparvaid@amazon.com>
2023-08-17 16:28:25 +0000

    updated

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,12 +1,12 @@
 module "s3_bucket" {
+  version = "2.8.0"
   source = "terraform-aws-modules/s3-bucket/aws"
-
   bucket = "my-s3-bucket-terraform"
   bucket_prefix = "${var.prefix}"
   acl    = "private"
 
   versioning = {
-    enabled = true
+    enabled = false
   }
 
 }
